30894fe84800fc6e1068bee4f89da5be829268fb
Franco Armando <francoarmando1911@gmail.com>
2024-11-03 10:18:46 -0300

    correcciones en header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
     const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
-        <header className="bg-gradient-to-r from-indigo-600 to-purple-600 shadow-lg">
+        <header className="bg-gradient-to-r from-red-600 to-red-900 shadow-lg">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex - items-center justify-between h-16">
                     <div className="flex items-center">
