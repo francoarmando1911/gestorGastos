e5021627a7048f73592eed02ba5f895de7342722
Franco Armando <francoarmando1911@gmail.com>
2024-11-04 18:14:31 -0300

    actualizacion de datos en componentes

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,13 @@
+/*
+
+FormComponent: Un formulario que permite a los usuarios ingresar los detalles del 
+gasto, como el nombre, categoría, monto y una descripción opcional. 
+Puedes estructurarlo de la siguiente manera:
+
+- InputField: Un componente reutilizable para los campos de texto y numéricos.
+- SelectCategory: Un componente de selección desplegable para las categorías.
+- Button: Un botón para enviar el formulario.
+- FormValidation: Un componente opcional que muestra mensajes de validación si los 
+campos no se completan correctamente.
+
+*/
